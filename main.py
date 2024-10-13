a321d536adb1b1920dc277e9c0f1458dedabcec4
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-10-13 22:58:39 +0900

    FastAPIエントリーポイント

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# main.py
 from fastapi import FastAPI
+from app.model import InputData
+from app.predict import make_prediction
 
+# FastAPIのインスタンスを作成
 app = FastAPI()
 
-@app.get('/')
-async def read_root():
-    return {"message": "hello world"}
+# 予測エンドポイント
+@app.post("/predict")
+async def predict(data: InputData):
+    return make_prediction(data)
